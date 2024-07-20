f67a92049d5b329ca489bfed8a25d24f05bf70e1
Mootassam <Mootassame@gmail.com>
2024-07-20 20:03:40 +0100

    add ten ew commit

---
+++ assets/Invitation-50108a91.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-c78ce0e0.js";import{S as s}from"./SubHeader-1c66c065.js";function r(){return e.jsxs("div",{children:[e.jsx(s,{title:"Events",path:"/"}),e.jsxs("div",{className:"detaill__company",style:{whiteSpace:"pre-line"},children:[e.jsx("img",{src:"/images/events/1.jpg"}),e.jsx("img",{src:"/images/events/2.jpg"})]})]})}export{r as default};
